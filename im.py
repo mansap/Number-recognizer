a17c6e03ee443212fd83c278efabb8af5b6ee124
Mansa Pabbaraju <mansaramdas@gmail.com>
2019-02-24 21:47:37 -0800

    Add files via upload

---
+++ im.py
@@ -0,0 +1,72 @@
+__author__ = 'Mansa'
+
+import Image
+import numpy as np
+from gtts import gTTS
+from collections import Counter
+
+
+def createExamples():
+    numberArrayExamples = open('numArEx.txt','a')
+    numbersWeHave = range(1,10)
+    for eachNum in numbersWeHave:
+        for furtherNum in numbersWeHave:
+            print(str(eachNum)+'.'+str(furtherNum))
+            imgFilePath = 'images/numbers/'+str(eachNum)+'.'+str(furtherNum)+'.png'
+            ei = Image.open(imgFilePath)
+            eiar = np.array(ei)
+            eiarl = str(eiar.tolist())
+
+            print(eiarl)
+            lineToWrite = str(eachNum)+'::'+eiarl+'\n'
+            numberArrayExamples.write(lineToWrite)
+
+
+def whatNumIsThis(filePath):
+
+    matchedAr = []
+    loadExamps = open('numArEx.txt','r').read()
+    loadExamps = loadExamps.split('\n')
+    i = Image.open(filePath)
+    iar = np.array(i)
+    iarl = iar.tolist()
+    inQuestion = str(iarl)
+
+    for eachExample in loadExamps:
+        try:
+            splitEx = eachExample.split('::')
+            currentNum = splitEx[0]
+            currentAr = splitEx[1]
+
+            eachPixEx = currentAr.split('],')
+            eachPixInQ = inQuestion.split('],')
+
+            x = 0
+
+            while x < len(eachPixEx):
+                if eachPixEx[x] == eachPixInQ[x]:
+                    matchedAr.append(int(currentNum))
+
+                x+=1
+        except IndexError :
+            pass
+
+    x = Counter(matchedAr)
+    print(x)
+    val = (x.most_common(1)[0][0])
+    print(val)
+    return val
+
+def textToSpeech(val):
+    tts = gTTS(text=str(val), lang='en')
+    tts.save("value.mp3")
+
+def main():
+
+    val = whatNumIsThis('images/test.png')
+    textToSpeech(val)
+
+
+
+
+
